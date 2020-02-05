5e3425375d9cc0cbe8a1810886adab1b275e6b25
moripeji <moripeji@gmail.com>
2020-02-05 16:31:14 -0500

    score manager creation/ implementation

--- exercises/angryBirdsHW/angryBirdsROTPpage/Assets/_Scripts/PlayerControl.cs
+++ exercises/angryBirdsHW/angryBirdsROTPpage/Assets/_Scripts/PlayerControl.cs
@@ -36,4 +36,5 @@ public class PlayerControl : MonoBehaviour
         }
 
     }
+
 }
---
+++ exercises/angryBirdsHW/angryBirdsROTPpage/Assets/_Scripts/ScoreManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public int score = 0;
+    const int StructureHitPoint = 1;
+    const int PiggyHitPoint = 5;
+    //public Text scoreText; //score counter
+
+    public static ScoreManager instance = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (instance == null)
+        {
+            instance = this; //do we have a GM yet? 
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void StructureSmash()
+    {
+        score = score + StructureHitPoint;
+        Debug.Log("Score: " + GetScore());
+        //scoreText.text = "Score: " + score;
+    }
+
+    public void PigSmashStructure()
+    {
+        score = score + PiggyHitPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
