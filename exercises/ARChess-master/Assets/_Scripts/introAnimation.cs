14e234d24eb39ffe26ccb6423c2c0446ababbf1a
moripeji <moripeji@gmail.com>
2020-02-17 17:01:57 -0500

    attempted pawn controller creation

---
+++ exercises/ARChess-master/Assets/_Scripts/introAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introAnimation : MonoBehaviour
+{
+    const string whitesPlay = "whitesPlay";
+    const string blacksPlay = "blacksPlay";
+
+    public Animator cameraAnimator; //can use this with triggers in animator controller now
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            cameraAnimator.SetTrigger(whitesPlay);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            cameraAnimator.SetTrigger(blacksPlay);
+        }
+    }
+}
---
+++ exercises/ARChess-master/Assets/_Scripts/pawnAnimation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pawnAnimation : MonoBehaviour
+{
+    public Animator pawnAnimator;
+
+    const string attackPawn = "attackPawn";
+    const string idlePawn = "idlePawn";
+    const string movePawn = "movePawn";
+    const string rotateRightPawn = "rotateRightPawn";
+    const string rotateLeftPawn = "rotateLeftPawn";
+    const string diePawn = "diePawn";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the player chooses A, pawn will attack.
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            pawnAnimator.SetTrigger(attackPawn);
+        }
+
+        //if the player chooses M, pawn will move forward.
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            pawnAnimator.SetTrigger(movePawn);
+        }
+
+        //if the player chooses R, pawn will rotate right.
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            pawnAnimator.SetTrigger(rotateRightPawn);
+        }
+
+        //if the player chooses L, pawn will rotate left.
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            pawnAnimator.SetTrigger(rotateLeftPawn);
+        }
+
+        //if the player does not choose anything, the pawn will default to idle
+        /*else
+        {
+            pawnAnimator.SetTrigger(idlePawn);
+        }*/
+    }
+}
