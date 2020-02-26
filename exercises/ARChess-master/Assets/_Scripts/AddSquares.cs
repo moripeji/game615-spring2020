7da9af62e43853ff9222349c62ee4c59388ba557
moripeji <moripeji@gmail.com>
2020-02-25 22:24:31 -0500

    added raycasting scripts and features to chess!!!! success!!!!

---
+++ exercises/ARChess-master/Assets/_Scripts/AddSquares.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddSquares : MonoBehaviour
+{
+    public GameObject originalSquare;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                GameObject square = Instantiate(originalSquare, transform);
+                square.transform.position = new Vector3(i * 2 - 7, 1.8f, j * 2 - 7);
+                square.GetComponent<MeshRenderer>().material.color = new Color(1, 0, 0, 0.0f);
+            }
+        }
+
+        Destroy(originalSquare);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ exercises/ARChess-master/Assets/_Scripts/InputController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    Transform selectedPiece;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void selectPiece(Transform piece)
+    {
+        selectedPiece = piece;
+
+        Renderer[] renderers = selectedPiece.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in renderers)
+        {
+            r.material.EnableKeyword("_EMISSION");
+            r.material.SetColor("_EmissionColor", Color.red);
+        }
+    }
+
+    void deselectPiece()
+    {
+        if (selectedPiece)
+        {
+            Renderer[] renderers = selectedPiece.GetComponentsInChildren<Renderer>();
+            foreach (Renderer r in renderers)
+            {
+                r.material.DisableKeyword("_EMISSION");
+                r.material.SetColor("_EmissionColor", Color.black);
+            }
+        }
+
+        selectedPiece = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform.tag == "square")
+                {
+                    hit.transform.GetComponent<MeshRenderer>().material.color = new Color(1, 0, 0, 0.3f);
+                    if (selectedPiece)
+                    {
+                        selectedPiece.position = hit.transform.position;
+                        deselectPiece();
+                       // selectedPiece.GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.black);
+                    }
+                }
+
+                if (hit.transform.tag == "piece")
+                {
+                    selectPiece(hit.transform);
+                }
+            }
+        }
+    }
+}
